d65db76d490763fddc046bf9a3ff4ca03ec3c799
지영진 <wldudwls12@gmail.com>
2022-10-23 01:18:15 +0900

    first commit

---
+++ READMD.md
@@ -0,0 +1,14 @@
+# Spring Batch
+
+Batch는 스케줄러가 아님. Batch Job을 관리하는 역할
+스케줄링은 따로 Scheduler, Quartz, Jenkins 등으로 해줘야함
+
+## 개발환경
+* JDK 1.8 이상
+* Spring Boot 2.5.1
+* DB - H2, Mysql
+* Lombok
+* IntelliJ
+  * Annotation Processors 설정 enable
+* Maven
+
---
+++ src/test/java/me/youngjin/inflearnspringbatch/InflearnSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.youngjin.inflearnspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
